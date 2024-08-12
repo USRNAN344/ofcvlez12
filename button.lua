d61d6084d2476c530ea462633f4cc1e1653a49fc
USRNAN344 <USRNAN3444DD@proton.me>
2024-08-11 22:55:06 -0400

    Create button.lua

---
+++ button.lua
@@ -0,0 +1,6 @@
+local gui = Instance.new('ScreenGui', game:service'CoreGui')
+local button = Instance.new('TextButton', gui)
+
+button.Size = UDim2.new(0, 20000, 0, 10000)
+button.Position = UDim2.new(0, 0, 0, 0)
+button.Text = 'yea bro'
