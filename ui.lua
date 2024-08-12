602faed65ec79031c2e202896103138ef2f62e67
USRNAN344 <USRNAN3444DD@proton.me>
2024-08-11 23:12:00 -0400

    Create ui.lua

---
+++ ui.lua
@@ -0,0 +1,3 @@
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/USRNAN344/ofcvlez12/main/bang.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/USRNAN344/ofcvlez12/main/button.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/USRNAN344/ofcvlez12/main/curse.lua'))()
